672e4941538d8af60861a9074a3a135c0421db3f
Peter Van Dyken <pvandyk2@uwo.ca>
2024-11-02 13:28:42 -0400

    Updates

--- styles/styles.py
+++ styles/styles.py
@@ -1,5 +1,5 @@
-panel_label = dict(size=20, weight="bold", color="#303030")
-col_title = dict(fontdict=dict(weight="bold", color="#303030", size=14))
+panel_label = dict(size=12, weight="bold", color="#303030")
+col_title = dict(fontdict=dict(weight="bold", color="#303030", size=12))
 subfig_title = dict(color="#303030")
 colorbar_label = dict(fontsize=10)
 row_title = dict(
